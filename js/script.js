7b017d10e690a1988d93a471fcc6ada59c4e7ce6
ColopanoE <155704600+ColopanoE@users.noreply.github.com>
2024-01-05 19:45:22 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,69 +1,41 @@
-// Select the element with the id 'menu-btn' and assign it to the variable 'menu'
 let menu = document.querySelector('#menu-btn');
-
-// Select the element with the class 'navbar' and assign it to the variable 'navbar'
 let navbar = document.querySelector('.navbar');
 
-// Add a click event listener to the 'menu' element
-menu.onclick = () => {
-    // Toggle the 'fa-times' class on the 'menu' element
+menu.onclick = () =>{
     menu.classList.toggle('fa-times');
-    
-    // Toggle the 'active' class on the 'navbar' element
     navbar.classList.toggle('active');
 }
 
-// Add a scroll event listener to the window
-window.onscroll = () => {
-    // Remove the 'fa-times' class from the 'menu' element
+window.onscroll = () =>{
     menu.classList.remove('fa-times');
-    
-    // Remove the 'active' class from the 'navbar' element
     navbar.classList.remove('active');
 };
 
-// Select all elements with the class 'image-slider img' and iterate over them
-document.querySelectorAll('.image-slider img').forEach(images => {
-    // Add a click event listener to each image element
-    images.onclick = () => {
-        // Get the 'src' attribute value of the clicked image
+document.querySelectorAll('.image-slider img').forEach(images =>{
+    images.onclick = () =>{
         var src = images.getAttribute('src');
-        
-        // Set the 'src' attribute value of the element with the class 'main-home-image' to the clicked image's 'src'
         document.querySelector('.main-home-image').src = src;
     };
 });
 
-// Create a new instance of the Swiper class with the specified options
 var swiper = new Swiper(".review-slider", {
-    // Set the space between slides to 20 pixels
     spaceBetween: 20,
-
-    // Configure pagination settings
     pagination: {
-        el: ".swiper-pagination",   // Pagination element selector
-        clickable: true,           // Enable clickable pagination
+      el: ".swiper-pagination",
+      clickable: true,
     },
-
-    // Enable looping through slides
-    loop: true,
-
-    // Show a grab cursor on hover
+    loop : true,
     grabCursor: true,
-
-    // Configure autoplay settings
     autoplay: {
-        delay: 7500,               // Delay between slides in milliseconds
-        disableOnInteraction: false, // Disable autoplay on user interaction
+        delay: 7500,
+        disableOnInteraction: false,
     },
-
-    // Configure responsive breakpoints
     breakpoints: {
         0: {
-            slidesPerView: 1,      // Number of slides to show on screen at 0px and above
+          slidesPerView: 1,
         },
         768: {
-            slidesPerView: 2,      // Number of slides to show on screen at 768px and above
+          slidesPerView: 2,
         },
     },
 });
